a7df9fce5044cb4f97dadb74b34c6e56d191fb2e
budnyinqa <budnyinqa@gmail.com>
2025-08-04 23:36:00 +0200

    Add files via upload

---
+++ Draggable Objects/Draggable Objects/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace Draggable_Objects
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.DarkRed;
+            this.label1.Location = new System.Drawing.Point(56, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 40);
+            this.label1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(56, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 40);
+            this.label2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Gold;
+            this.label3.Location = new System.Drawing.Point(56, 188);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 40);
+            this.label3.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Draggable Objects/Draggable Objects/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Draggable_Objects
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            foreach (Control control in Controls)
+            {
+                if (control is Label)
+                {
+                    // Initialize methods responsible for handling object movement 
+                    // or their overlapping
+                    control.MouseMove += new MouseEventHandler(ObjectMove);
+                    control.MouseDown += new MouseEventHandler(MouseAction);
+                }
+            }
+        }
+        // Method that changes the position of the object
+        private void ObjectMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Label box = sender as Label;
+                // When the user holds the left mouse button and moves the cursor,
+                // the object also moves based on the control’s position saved in its tag
+                box.Left += e.X - ((Point)box.Tag).X;
+                box.Top += e.Y - ((Point)box.Tag).Y;
+            }
+        }
+        // Method handles the mouse down event, reacting to mouse clicks
+        private void MouseAction(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Label box = sender as Label;
+                box.Tag = e.Location; // When the user clicks the left mouse button, the cursor position is saved
+            }
+            if (e.Button == MouseButtons.Right)
+            {
+                Label box = sender as Label;
+                box.BringToFront();
+            }
+        }
+    }
+}
